d6093356b7217f0a8c470b14b8e3a340c488173f
Fadil <43397338+adilpiliang@users.noreply.github.com>
2024-05-27 02:07:26 +0700

    27052024

--- resources/views/user/article.blade.php
+++ resources/views/user/article.blade.php
@@ -12,32 +12,68 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-QHpuNzP5tkJ/NlTrEZKN5xubtkopxHJJu6CNKxtEVcGUg5/cDDzVYydWct8lIUkc2Y9EfmUHHdTVZiBe0kxO0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <style>
-        .navbar-nav .nav-link {
-            color: white;
-            font-family: 'Open Sans', sans-serif;
-            font-size: 18px;
+        .container {
+            padding: 20px;
+            display: flex;
         }
 
-        .article_container {
-            max-height: 300px;
-            max-width: 200px;
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+
+        .col-md-4 {
+            flex: 1 1 30%;
+            max-width: 30%;
+            margin: 10px;
+        }
+
+        .card {
+            border: none;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+
+        .card-img-top {
+            width: 100%;
+            height: 200px;
+            object-fit: cover;
+        }
+
+        .card-title {
+            font-size: 1.25rem;
+            margin-bottom: 0.75rem;
+        }
+
+        .card-text {
+            font-size: 1rem;
+            margin-bottom: 1.25rem;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
         }
     </style>
 </head>
 
 <body>
     @include('navbar')
-    <div class="article_container">
-        @foreach ($articles as $articles)
-        <div class="item">
-            <img src="img/upload/article/{{$articles-> image}}" alt="Profil" width="180">
-            <h2 style="font-size: 20px;">{{ $articles->title }}</h2>
-            <p class="price">{{ $articles->content }}</p>
-            <div class="">
-                <a class="keranjang-btn">Keranjang</a>
+    <div class="container">
+        <div class="row">
+            @foreach($articles as $articles)
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="img/upload/article/{{$articles-> image}}" class="card-img-top" alt="{{ $articles->title }}" style="height: 200px; object-fit: cover;">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $articles->title }}</h5>
+                        <p class="card-text">{{ Str::limit($articles->content, 100) }}</p>
+                        <a href="#" class="btn btn-primary">Read More</a>
+                    </div>
+                </div>
             </div>
+            @endforeach
         </div>
-        @endforeach
     </div>
 
 </body>
