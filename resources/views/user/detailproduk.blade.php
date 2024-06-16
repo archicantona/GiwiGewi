733e588b6112800d37c3b91bff29b8606bec5f80
archicantona <archicantona7@gmail.com>
2024-06-16 16:11:00 +0700

    p

--- resources/views/user/detailproduk.blade.php
+++ resources/views/user/detailproduk.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Peoduk Detail</title>
+    <title>Produk Detail</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
